3306e4e27805e400c6a5f21add9c6ed06d8d22f4
pedro-mesquitaa <pedrovitornogueiram1@gmail.com>
2025-01-22 13:49:11 -0300

    adding task listing method

--- lib/src/core/archives.dart
+++ lib/src/core/archives.dart
@@ -1,1 +1,16 @@
+import 'dart:convert';
+import 'dart:io';
+
 save(){}
+
+Future<Map<String, dynamic>> reciveArchive(String archive) async {
+  
+  if (archive.contains("task.json")) {
+    final file = File("../config/task.json");
+    final jsonString = await file.readAsString();
+
+    return json.decode(jsonString);
+  } else {
+    return <String, dynamic>{};
+  }
+}
--- lib/src/core/tasksOperations.dart
+++ lib/src/core/tasksOperations.dart
@@ -1,7 +1,17 @@
+import 'package:to_do_App/src/core/archives.dart';
 
 addTask() {}
 
-listTask() {}
+listTask() async {
+  Map<String, dynamic> tasks = await reciveArchive("../config/task.json");
+
+
+  if (tasks.isEmpty) {
+    print("no tasks found");
+  } else {
+    tasks.forEach((key, value) => print("[$key]: $value"));
+  }
+}
 
 checkTask(int index) {}
 
